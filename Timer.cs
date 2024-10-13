214f4354a19e67797264da49c1b003d8c382b493
krhenningham <khenningham7@gmail.com>
2024-10-13 04:23:01 -0400

    Update Timer.cs

--- Timer.cs
+++ Timer.cs
@@ -3,41 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float timer;
-    public Text timerText;
-    public int candyCount = 100;
-    //public Camera mainCam;
-    //public Camera rankCam;
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
     
-    void Start()
+    
+    /*void Start()
     {
-        //mainCam.SetActive(true);
-        //rankCam.SetActive(false);
+    
         timerText = GetComponent<Text>();
-        //rankCam = GameObject.FindGameObjectWithTag("Rank").GetComponent<Camera>();
-    }
+        
+    } */
 
     // Update is called once per frame
     void Update()
     {
-        timer -= Time.deltaTime;
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime <= 0)
+        {
+            Time.timeScale = 0;
+            remainingTime = 0;
+            SceneManager.LoadSceneAsync(2);
+        }
+
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        /*timer -= Time.deltaTime;
         timerText.text = timer.ToString();
         if (timer <= 0)
         {
-            //Application.Quit();
+            
             timerText.text = "Candy Count: " + candyCount.ToString();
             Time.timeScale = 0;
             timer = 0; // Clamp the timer to zero
             return;
-            //timerText.text = "0"; // Show zero on the UI
-            //timerText.text = "'" + candyCount + "'"; // Show zero on the UI
-            //mainCam.SetActive(false);
-            //rankCam.SetActive(true);
-        }
+        } */
     }
 
 }
